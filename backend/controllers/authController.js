aa21571f4275fa5411842e917bb9bcbfdf0df713
tadanhthang <tadanhthang2004@gmail.com>
2025-04-02 14:53:08 +0700

    sever

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { poolPromise } = require('../config/dbConfig');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const router = express.Router();
